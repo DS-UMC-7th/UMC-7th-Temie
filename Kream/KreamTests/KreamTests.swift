6e59a2eb1d944fae70144f72917ecba6b02d6deb
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-10-08 20:00:35 +0900

    initail commit

---
+++ Kream/KreamTests/KreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  KreamTests.swift
+//  KreamTests
+//
+//  Created by narong on 10/6/24.
+//
+
+import Testing
+@testable import Kream
+
+struct KreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
